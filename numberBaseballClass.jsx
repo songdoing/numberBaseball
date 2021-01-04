b481fee5538df0a6d68e588f7a485694053ea088
wonjinsong <msmk22239@gmail.com>
2021-01-04 17:05:59 -0500

    numberBaseball Class

--- numberBaseballClass.jsx
+++ numberBaseballClass.jsx
@@ -13,10 +13,33 @@ class NumberBaseball extends Component {
         tries :[]
     };
 
+    onSubmitForm = (e) => {
+
+    };
+    
+    onChangeInput = (e) => {
+    
+    };
+    
+    input;
+    
+    onRefInput = (c) => {
+        this.input = c;
+    };
+
     render() {
         return (
             <>
-            
+            <h1>Guess 4 digit numbers.</h1>
+            <h2>{this.state.result}</h2>
+            <form onSubmit={this.onSubmitForm}>
+                <input maxLength={4} ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+                <button>ENTER</button>
+            </form>
+            <div>TRY : {this.state.tries.length}</div>
+            <ul>
+                <li />
+            </ul>
             </>
         );
     }
