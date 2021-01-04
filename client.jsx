80580fd114ea14ee064db791f02ce90d06c2fbf5
wonjinsong <msmk22239@gmail.com>
2021-01-04 17:03:00 -0500

    webpack configuration

---
+++ client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import NumberBaseball from './numberBaseballClass';
+
+ReactDom.render(<NumberBaseball/>, document.querySelector('#root'));
---
+++ numberBaseballClass.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+//get numbers without duplication
+function getNumbers() {
+
+}
+
+class NumberBaseball extends Component {
+    state = {
+        result : '',
+        value : '',
+        answer : getNumbers(),
+        tries :[]
+    };
+
+    render() {
+        return (
+            <>
+            
+            </>
+        );
+    }
+}
+
+export default NumberBaseball;
